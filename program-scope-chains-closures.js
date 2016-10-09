f9c0b80268d8ac51ab3ecd5d5850af7ebc004cf9
Adam Urbanik <adamurbanik@gmail.com>
2016-10-09 14:33:30 +0200

    scope chains closures

---
+++ program-scope-chains-closures.js
@@ -0,0 +1,42 @@
+// ********* SCOPE CHAINS CLOSURES ****** /////
+
+//1
+// function foo() {
+//   var bar;
+// }
+
+
+//2
+// function foo() {
+//   var bar;
+//   function zip() {
+//     var quux;
+//   }
+// }
+
+
+//3
+// function foo() {
+//   var bar;
+//   quux = 0;
+//   function zip() {
+//     var quux = 1;
+//   }
+// }
+
+
+//4
+// function foo() {
+//   var bar;
+//   quux = 0;
+  
+//   function zip() {
+//     var quux = 1;
+//     bar = true;
+//   }
+
+//   return zip;
+// }
+
+
+
